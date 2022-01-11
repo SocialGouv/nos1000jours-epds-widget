101c7a46ca1409137348290fe010eb198bd0b131
Audrey Lebret <alebret@ippon.fr>
2022-01-11 15:51:48 +0100

    feat(demarrage): écran de démarrage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css"
 import { Col } from "react-bootstrap"
+import { } from "@dataesr/react-dsfr"
+import Head from "next/head"
 
 export default function Home() {
+
+  const startSurvey = () => {
+    console.log("START")
+    /**
+     * -> event matoto
+     * -> nom du canal (ex : SiteWebPro, Appli1000j ... ou utilisateur du widget, mettre le nom du canal dans les paramètre du widget)
+     */
+  }
+
   return (
     <div className={styles.container}>
-      <Col>Hello world</Col>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Questioinnaire EPDS" />
+        <link rel="icon" href="/img/logo-1000j.svg" />
+      </Head>
+
+      <div className={styles.main}>
+        <img
+          src="/img/logo-1000j.svg"
+          height={75}
+          style={{ margin: 15 }}
+          alt="Logo 1000 premiers jours"
+        />
+        <Col>Se tester c&#39;est déjà se soigner</Col>
+        <br />
+        <button className="fr-btn fr-btn--lg" onClick={startSurvey}>
+          COMMENCER LE TEST
+        </button>
+      </div>
     </div>
   )
 }
