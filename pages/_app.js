848f0a011879ea01d73d2e3c10b4f7bd1546d4e2
Audrey Lebret <alebret@ippon.fr>
2022-01-19 17:33:02 +0100

    feat(resultats): ajout de la font Marianne

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,16 @@ import "../styles/colors.scss"
 
 import { init } from "@socialgouv/matomo-next"
 import App from "next/app"
+import React from "react"
+import { ThemeProvider } from "react-bootstrap"
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
 const MATOMO_ENABLED = process.env.NEXT_PUBLIC_MATOMO_ENABLED
 
+const theme = {
+  font: "Marianne",
+}
 class MyApp extends App {
   componentDidMount() {
     if (MATOMO_ENABLED === "true")
@@ -17,7 +22,13 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+    return (
+      <React.Fragment>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </React.Fragment>
+    )
   }
 }
 
