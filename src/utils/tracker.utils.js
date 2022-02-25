00494fc435fea2c8ea4ecdab09476f57c0038edc
Audrey Lebret <alebret@ippon.fr>
2022-02-25 15:32:25 +0100

    feat(matomo): confirmation d'envoi

--- src/utils/tracker.utils.js
+++ src/utils/tracker.utils.js
@@ -6,6 +6,11 @@ export const CATEG = {
   survey: "Questionnaire",
 }
 
+export const CONTACT_SENT = {
+  mail: "confirmation_mail",
+  sms: "confirmation_sms",
+}
+
 export const trackerClick = (categ, action, name) => {
   if (process.env.NEXT_PUBLIC_MATOMO_ENABLED === "true")
     _paq.push(["trackEvent", categ, action, name])
