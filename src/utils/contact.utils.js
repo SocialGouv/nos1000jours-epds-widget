192bba4b0990975a8e7dff562a0290d1de1575f8
Lebret Audrey <alebret@ippon.fr>
2022-11-22 18:01:03 +0100

    fix(matomo): retouurs de PR

--- src/utils/contact.utils.js
+++ src/utils/contact.utils.js
@@ -5,22 +5,20 @@ import { RequestContact } from "../constants/constants"
  * @param {RequestContact.type} contactType
  */
 export const sendTrackerContactConfirmed = (contactType) => {
-  let name
-  switch (contactType) {
-    case RequestContact.type.email:
-      name = TrackerUtils.CONTACT_SENT.mail
-      break
-    case RequestContact.type.sms:
-      name = TrackerUtils.CONTACT_SENT.sms
-      break
-    case RequestContact.type.chat:
-      name = TrackerUtils.CONTACT_SENT.chat
-      break
-  }
-
   TrackerUtils.trackerClick(
     TrackerUtils.CATEG.contact,
     TrackerUtils.ACTION.contact_confirm_sent,
-    name
+    trackerContactName(contactType)
   )
+}
+
+const trackerContactName = (contactType) => {
+  switch (contactType) {
+    case RequestContact.type.email:
+      return TrackerUtils.CONTACT_SENT.mail
+    case RequestContact.type.sms:
+      return TrackerUtils.CONTACT_SENT.sms
+    case RequestContact.type.chat:
+      return TrackerUtils.CONTACT_SENT.chat
+  }
 }
