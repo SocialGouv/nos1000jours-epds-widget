327f3af646305ad1fd427ea1fb821d331ff51046
Lebret Audrey <alebret@ippon.fr>
2022-12-02 15:53:25 +0100

    fix(matomo):  ajout tracker maman blues

--- src/components/results/ContactMamanBlues.js
+++ src/components/results/ContactMamanBlues.js
@@ -24,6 +24,7 @@ export function ContactMamanBlues({ scoreLevel }) {
     default:
       break
   }
+  AbTestingUtils.trackerForAbTesting(`Macaron Elise - ${colorsByLevel}`)
 
   const goToBeContacted = async (event) => {
     TrackerUtils.tracker(
