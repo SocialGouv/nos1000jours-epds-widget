43ccaa5399418376266d6fdbb840ef8c224b6e49
Lebret Audrey <alebret@ippon.fr>
2023-01-04 10:36:05 +0100

    feat(questionnaire): supprimer l'import de la lib du dsfr

--- src/components/survey/SurveyProgressBar.js
+++ src/components/survey/SurveyProgressBar.js
@@ -1,16 +1,22 @@
 import React from "react"
-import { Stepper } from "@codegouvfr/react-dsfr/Stepper"
 
 export function SurveyProgressBar({ indexNow, size }) {
   // TODO: bug sur le composant : https://gouvfr.atlassian.net/servicedesk/customer/portal/1/DSFR-1125
 
   return (
     <div className="survey-progressbar">
-      <Stepper
-        currentStep={indexNow}
-        stepCount={size}
-        title={`Question ${indexNow}`}
-      />
+      <div className="fr-stepper">
+        <h2 className="fr-stepper__title">
+          <span claclassNamess="fr-stepper__state">
+            Question {indexNow} sur {size}
+          </span>
+        </h2>
+        <div
+          className="fr-stepper__steps"
+          data-fr-current-step={indexNow}
+          data-fr-steps={size}
+        ></div>
+      </div>
     </div>
   )
 }
