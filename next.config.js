e00f8d5193ca952f560437c1d717d70ed6589804
Lebret Audrey <alebret@ippon.fr>
2023-01-06 15:03:21 +0100

    fix(dsfr): next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["@codegouvfr/react-dsfr"])
+const withTM = require('next-transpile-modules')(['@codegouvfr/react-dsfr'])
 
 module.exports = withTM({
   reactStrictMode: true,
