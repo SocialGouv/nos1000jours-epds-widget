c71684177bf68f0d8d6a7ad446fbefdc9acd4259
Lebret Audrey <alebret@ippon.fr>
2023-01-09 14:46:00 +0100

    feat(matomo): trackers génériques

--- src/utils/tracker.utils.js
+++ src/utils/tracker.utils.js
@@ -13,10 +13,18 @@ export const CATEG = {
   results: "Résultats",
 }
 
+export const ACTION_GENERIQUE = "Générique"
 export const ACTION = {
   contact_confirm_sent: "Confirmation d'envoi de la demande de contact",
   contact_type: "Choix du type de prise de contact",
   parcours: "Parcours",
+  generic: ACTION_GENERIQUE,
+}
+
+export const NAME = {
+  start: "Commencer",
+  end: "Terminer",
+  contact_confirm_sent: "Confirmation d'envoi de la demande de contact",
 }
 
 export const CONTACT_SENT = {
@@ -29,4 +37,13 @@ export const CONTACT_SENT = {
 export const track = (categ, action, name) => {
   if (process.env.NEXT_PUBLIC_MATOMO_ENABLED === "true")
     push(["trackEvent", categ, action, name])
+}
+
+/**
+ * Tracker générique (action "generique") pour pouvoir utiliser faciliter les stats sans information précise (test, source)
+ * @param {CATEG} categ
+ * @param {*} name
+ */
+export const genericTracker = (categ, name) => {
+  track(categ, ACTION.generic, name)
 }
