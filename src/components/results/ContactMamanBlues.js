eecb12dccc37e48730fc910b42b496a46410a8f1
Lebret Audrey <alebret@ippon.fr>
2023-01-16 10:32:42 +0100

    feat(abtesting): suppression sur le bt contacter

--- src/components/results/ContactMamanBlues.js
+++ src/components/results/ContactMamanBlues.js
@@ -3,13 +3,12 @@ import { Row } from "react-bootstrap"
 import { } from "@dataesr/react-dsfr"
 import { useRouter } from "next/router"
 import * as TrackerUtils from "../../utils/tracker.utils"
-import * as ContactButtonLabelAbTesting from "../../utils/ab-testing/contact-button-label.utils"
 import * as AbTestingUtils from "../../utils/ab-testing/ab-testing.utils"
 
 export function ContactMamanBlues({ scoreLevel }) {
   const router = useRouter()
+  const buttonLabel = "Je veux être accompagné.e"
   let colorsByLevel
-  const buttonLabel = ContactButtonLabelAbTesting.getContactButtonLabelByTest()
 
   switch (scoreLevel) {
     case 1:
@@ -29,9 +28,8 @@ export function ContactMamanBlues({ scoreLevel }) {
     TrackerUtils.track(
       TrackerUtils.CATEG.contact,
       `Macaron d'Elise ${TrackerUtils.EVENT_CLICK}`,
-      "Être contacté(e)"
+      { buttonLabel }
     )
-    AbTestingUtils.trackerForAbTesting(buttonLabel)
 
     router.push({
       pathname: "/contact/to-be-contacted",
