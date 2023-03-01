88b404b0bcd268810e0bfcb1d63f176876ae82cd
mbarry-ippon <mbarry@ippon.fr>
2023-03-01 14:41:02 +0100

    fix: remove unused useEffect import

--- src/components/results/ContactMamanBlues.js
+++ src/components/results/ContactMamanBlues.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Row } from "react-bootstrap"
 import {} from "@dataesr/react-dsfr"
 import { useRouter } from "next/router"
