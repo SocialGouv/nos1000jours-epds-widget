fe3f8617105b51ac4eaf423c29d33a0d1d7c89f1
mbarry-ippon <mbarry@ippon.fr>
2023-07-11 07:28:18 +0200

    fix: remove doctolib bespoke screen

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { Container, Row } from "react-bootstrap"
-import { STORAGE_SOURCE } from "../constants/constants"
-import * as StorageUtils from "../utils/storage.utils"
+
 export function Layout({ children }) {
   return (
     <React.Fragment>
@@ -11,27 +10,9 @@ export function Layout({ children }) {
 }
 
 export function ContentLayout({ children }) {
-  const source = StorageUtils.getInLocalStorage(STORAGE_SOURCE)
-  const [height, setHeight] = useState("0px")
-
-  useEffect(() => {
-    if (source === "doctolib") {
-      setHeight("0px")
-    } else {
-      setHeight("80vh")
-    }
-  }, [height])
-
   return (
     <Layout>
-      <Row
-        style={{
-          alignContent: "start",
-          minHeight: height,
-        }}
-      >
-        {children}
-      </Row>
+      <Row style={{ alignContent: "start", minHeight: "80vh" }}>{children}</Row>
     </Layout>
   )
 }
